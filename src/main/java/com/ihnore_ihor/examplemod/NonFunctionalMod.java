62442cb4da09897a15f1cda562031c03c8071576
ihnore-ihor <igorbrajchenko@gmail.com>
2025-01-23 12:30:27 +0200

    reformatted code to java style

--- src/main/java/com/ihnore_ihor/examplemod/NonFunctionalMod.java
+++ src/main/java/com/ihnore_ihor/examplemod/NonFunctionalMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.ihnore_ihor.examplemod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,44 +13,37 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(NonFunctionalMod.MODID)
-public class NonFunctionalMod
-{
+public class NonFunctionalMod {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "NonFunctionalModification";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public NonFunctionalMod(FMLJavaModLoadingContext context)
-    {
+    public NonFunctionalMod(FMLJavaModLoadingContext context) {
 
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
 
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
 
         }
     }
